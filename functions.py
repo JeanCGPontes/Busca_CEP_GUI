c0341a255ff84044dcb378fbec7a7e6db105b46f
Jean CGP <jeanestudos.je@gmail.com>
2021-08-20 20:29:49 -0300

    FUNCTIONS

---
+++ functions.py
@@ -0,0 +1,46 @@
+import json
+
+import requests
+
+URL = "https://viacep.com.br/ws/{}/json/"
+
+
+def get_response_via_cep(cep_number):
+    try:
+        response = requests.get(URL.format(cep_number))
+        if response.status_code == 200:
+            return response.text
+
+    except Exception as error:
+        print(error)
+
+
+def return_parsing_json(json_text):
+    parsing = json.loads(json_text)
+    return parsing
+
+
+def returned_objects(parsing_response):
+    cep = parsing_response['cep']
+    address = parsing_response['logradouro']
+    district = parsing_response['bairro']
+    city = parsing_response['localidade']
+    state = parsing_response['uf']
+    return cep, address, district, city, state
+
+
+def validation_cep(cep_number):
+    if len(cep_number) == 8:
+        try:
+            cep_number = int(cep_number)
+            if cep_number:
+                return cep_number
+
+        except ValueError:
+            return 'Digite apenas números!'
+
+    elif len(cep_number) <= 8:
+        return 'Digite 8 números!'
+
+    elif len(cep_number) >= 8:
+        return 'Digite apenas 8 números!'
